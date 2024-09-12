0db53982dfe35993718b4e839a7c7ece90754a09
keerthana26092004 <123159818+keerthana26092004@users.noreply.github.com>
2024-09-12 19:24:45 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,147 @@
+export const JOBS = [
+    {
+        "id": 1,
+        "title": "Software Engineer",
+        "company": "Tech Innovations Inc.",
+        "salary": "$120,000",
+        "location": "New York, NY",
+        "description": "Develop and maintain software applications. Collaborate with cross-functional teams to deliver high-quality software solutions.",
+        "category": "Engineering",
+        "image": "https://uploads.sarvgyan.com/2016/04/career-in-software-engineering.jpg",
+        "rating": {
+            "rate": 4.5,
+            "count": 30
+        }
+    },
+    {
+        "id": 2,
+        "title": "Marketing Manager",
+        "company": "Creative Solutions Ltd.",
+        "salary": "$95,000",
+        "location": "San Francisco, CA",
+        "description": "Oversee marketing campaigns, develop strategies to increase brand awareness, and manage marketing teams.",
+        "category": "Marketing",
+        "image": "https://www.yourfreecareertest.com/wp-content/uploads/2016/07/marketing_manager-300x200.jpg",
+        "rating": {
+            "rate": 4.2,
+            "count": 25
+        }
+    },
+    {
+        "id": 3,
+        "title": "Graphic Designer",
+        "company": "Creative Agency",
+        "salary": "$65,000",
+        "location": "New York, NY",
+        "description": "Create visual content for print and digital media, develop design concepts, and collaborate with clients to meet their branding needs.",
+        "category": "Design",
+        "image": "https://www.doola.com/wp-content/uploads/2023/06/side-hustles-for-graphc-designers.jpg",
+        "rating": {
+            "rate": 4.5,
+            "count": 20
+        }
+    }
+    ,
+    {
+        "id": 4,
+        "title": "Product Manager",
+        "company": "Product Masters Ltd.",
+        "salary": "$110,000",
+        "location": "Chicago, IL",
+        "description": "Lead the development and management of products. Coordinate with teams to ensure products meet market needs and company goals.",
+        "category": "Product Management",
+        "image": "https://images.ctfassets.net/xri6xnn81z4a/7jdLCa5xoAiTKltPhAuJdu/3f2727214b7b3b644485de91cd3a6ebc/Product-Manager.jpg",
+        "rating": {
+            "rate": 4.3,
+            "count": 22
+        }
+    },
+    {
+        "id": 5,
+        "title": "Technical Support Specialist",
+        "company": "Tech Solutions",
+        "salary": "$55,000",
+        "location": "San Francisco, CA",
+        "description": "Provide technical support to clients, troubleshoot issues, and assist with software and hardware problems.",
+        "category": "Support",
+        "image": "https://facts.net/wp-content/uploads/2023/09/13-surprising-facts-about-technical-support-specialist-1695703828.jpg",
+        "rating": {
+            "rate": 4.2,
+            "count": 18
+        }
+    },
+    
+    {
+        "id": 6,
+        "title": "Sales Executive",
+        "company": "Sales Excellence Inc.",
+        "salary": "$75,000",
+        "location": "Los Angeles, CA",
+        "description": "Drive sales efforts, manage client relationships, and achieve sales targets. Develop strategies to expand market presence.",
+        "category": "Sales",
+        "image": "https://strapi.caseyinterview.com/uploads/How_to_assess_and_hire_Sales_Executive_595d6d7d57.png",
+        "rating": {
+            "rate": 4.0,
+            "count": 20
+        }
+    },
+    {
+        "id": 7,
+        "title": "HR Manager",
+        "company": "HR Solutions Group",
+        "salary": "$85,000",
+        "location": "Boston, MA",
+        "description": "Manage HR functions including recruitment, employee relations, and performance management. Ensure compliance with labor laws and company policies.",
+        "category": "Human Resources",
+        "image": "https://icehrm.com/blog/content/images/2021/06/pexels-rebrand-cities-1367272.jpg",
+        "rating": {
+            "rate": 4.6,
+            "count": 25
+        }
+    },
+    {
+        "id": 8,
+        "title": "Data Analytics Specialist",
+        "company": "Osswal Info",
+        "salary": "$80,000",
+        "location": "Remote",
+        "description": "Analyze and interpret complex data to help businesses make informed decisions. Utilize advanced analytics tools and techniques to drive strategic insights.",
+        "category": "Analytics",
+        "image": "https://osswalinfo.com/wp-content/uploads/2024/06/How-To-Take-Your-Data-Analytics-Approach-To-The-Next-Level-in-2023.jpg",
+        "rating": {
+            "rate": 4.2,
+            "count": 25
+        }
+    },
+    {
+        "id": 9,
+        "title": "Web Developer",
+        "company": "Tech Innovations",
+        "salary": "$80,000",
+        "location": "New York, NY",
+        "description": "Develop and maintain web applications, write clean and efficient code, and collaborate with designers and other developers to deliver high-quality products.",
+        "category": "Technology",
+        "image": "https://bairesdev.mo.cloudinary.net/blog/2023/09/How-Many-Web-Developers-in-the-World-1.jpg",
+        "rating": {
+            "rate": 4.5,
+            "count": 20
+        }
+    }
+    ,
+    {
+        "id": 10,
+        "title": "Operations Manager",
+        "company": "Dynamic Solutions",
+        "salary": "$80,000",
+        "location": "New York, NY",
+        "description": "Oversee daily operations, manage staff, and ensure efficient processes within the organization.",
+        "category": "Management",
+        "image": "https://online.maryville.edu/wp-content/uploads/sites/97/2023/09/operations-manager-1.jpg",
+        "rating": {
+            "rate": 4.5,
+            "count": 22
+        }
+    }
+    
+    
+];
